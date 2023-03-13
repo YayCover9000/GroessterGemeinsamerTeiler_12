833a63112c3ac7f09af3f395099dd873a233548c
YayCover9000 <yassuanf@googlemail.com>
2023-03-13 20:40:55 +0100

    GgT berechnung added

--- src/GgT.java
+++ src/GgT.java
@@ -4,15 +4,40 @@ public class GgT {
         int [] Array = new int[6];
         int zwischenEins;
         int zwischenZwei = 0;
+        int zwischenZwischen;
+        boolean notWhile = false;
+        int einsWhile;
+        int zweiWhile;
+        int randomNumb;
         for (int i = 0; i < Array.length; i++) {
-            Array[i] = (int)(Math.random() * 100);
+            randomNumb = (int)(Math.random() * 100);
+            if (randomNumb == 0) {
+                randomNumb =+1;
+            } else if(randomNumb == 100) {
+                randomNumb =-1;
+            }
+            Array[i] = randomNumb;
             System.out.println(Array[i]);
         }
         for (int j = 0; j < Array.length-1;) {
             zwischenEins = Array[j];
             j++;
             zwischenZwei = Array[j];
-            System.out.println("Test: " + zwischenEins + "\t" + zwischenZwei);
+            // System.out.println("Test: " + zwischenEins + "\t" + zwischenZwei);
+            einsWhile = zwischenEins;
+            zweiWhile = zwischenZwei;
+            // GGT Rechner
+            while (!notWhile) {
+                zwischenZwischen = einsWhile % zweiWhile;
+                einsWhile = zweiWhile;
+                zweiWhile = zwischenZwischen;
+                // System.out.println("zwischenZwischen: " + zwischenZwischen);
+                if (zwischenZwischen == 0) {
+                    notWhile = true;
+                    System.out.println("GgT von: " + zwischenEins + " und " + zwischenZwei + " ist: " + zwischenZwischen);
+                }
+            }
+            notWhile = false;
         }
 
     }
